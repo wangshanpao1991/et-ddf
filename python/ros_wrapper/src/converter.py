2e5b564459a0f2624819bf20e1de807928f8d8c5
Luke Barbier <lukebarbier12@gmail.com>
2019-12-12 14:09:40 -0700

    noise added in meas pub

--- python/ros_wrapper/src/converter.py
+++ python/ros_wrapper/src/converter.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TwistStamped
 
 class Converter:
     def __init__(self):
         rospy.Subscriber("/turtle1/cmd_vel", Twist, self.callback)
-        self.pub = rospy.Publisher("/ava/new_twist", Twist, queue_size=10)
+        self.pub = rospy.Publisher("/ava/new_twist", TwistStamped, queue_size=10)
         self.internal_x = 0
         self.internal_y = 0
 
@@ -13,9 +13,9 @@ class Converter:
         self.internal_x += msg.linear.x * (0.5) * 0.1
         self.internal_y += msg.angular.z * 0.5 * 0.1
 
-        t = Twist()
-        t.linear.x = self.internal_x
-        t.linear.y = self.internal_y
+        t = TwistStamped()
+        t.twist.linear.x = self.internal_x
+        t.twist.linear.y = self.internal_y
         self.pub.publish(t)
 
 if __name__ == "__main__":
--- python/ros_wrapper/src/point_sim.py
+++ python/ros_wrapper/src/point_sim.py
@@ -12,6 +12,7 @@ import tf
 
 ODOM_INDEX = 0
 PUB_INDEX = 1
+NOISE_INDEX = 2
 
 class PointSim:
 
@@ -36,8 +37,9 @@ class PointSim:
             start_odom.header.stamp = rospy.get_rostime()
             start_odom.header.frame_id = 'world'
             start_odom.child_frame_id = auv + '/base_link'
-            pub = rospy.Publisher(auv + '/pose_gt', Odometry, queue_size=10)
-            self.auvs[auv] = [start_odom, pub]
+            truth_pub = rospy.Publisher(auv + '/pose_gt', Odometry, queue_size=10)
+            noise_pub = rospy.Publisher(auv + '/pose_noise', Odometry, queue_size=10)
+            self.auvs[auv] = [start_odom, truth_pub, noise_pub]
             rospy.Subscriber(auv + '/new_twist', TwistStamped, self.control_callback)
 
     def load_start_pose(self, pose_list):
@@ -75,6 +77,7 @@ class PointSim:
 
     def update_poses(self, msg):
         for auv in self.auvs:
+            noise_odom = Odometry()
             odom = self.auvs[auv][ODOM_INDEX]
             roll, pitch, yaw = tf.transformations.euler_from_quaternion(( odom.pose.pose.orientation.x, \
                                                                           odom.pose.pose.orientation.y, \
@@ -84,15 +87,30 @@ class PointSim:
             roll += odom.twist.twist.angular.x * self.update_period
             pitch += odom.twist.twist.angular.y * self.update_period
             yaw += odom.twist.twist.angular.z * self.update_period
+
+            noise_roll = roll + np.random.normal(0, np.pi / 10)
+            noise_pitch = pitch + np.random.normal(0, np.pi / 10)
+            noise_yaw = yaw + np.random.normal(0, np.pi / 10)
+
             roll = self.correct_angles(roll)
             pitch = self.correct_angles(pitch)
             yaw = self.correct_angles(yaw)
+
+            noise_roll = self.correct_angles(noise_roll)
+            noise_pitch = self.correct_angles(noise_pitch)
+            noise_yaw = self.correct_angles(noise_yaw)
 
             quat_list = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
             odom.pose.pose.orientation.x = quat_list[0]
             odom.pose.pose.orientation.y = quat_list[1]
             odom.pose.pose.orientation.z = quat_list[2]
             odom.pose.pose.orientation.w = quat_list[3]
+
+            noise_quat_list = tf.transformations.quaternion_from_euler(noise_roll, noise_pitch, noise_yaw)
+            noise_odom.pose.pose.orientation.x = noise_quat_list[0]
+            noise_odom.pose.pose.orientation.y = noise_quat_list[1]
+            noise_odom.pose.pose.orientation.z = noise_quat_list[2]
+            noise_odom.pose.pose.orientation.w = noise_quat_list[3]
             
             odom.pose.pose.position.x += odom.twist.twist.linear.x * self.update_period
             odom.pose.pose.position.y += odom.twist.twist.linear.y * self.update_period
@@ -100,8 +118,15 @@ class PointSim:
 
             odom.header.stamp = rospy.get_rostime()
 
+            noise_odom.pose.pose.position.x += odom.pose.pose.position.x + np.random.normal(0, .1)
+            noise_odom.pose.pose.position.y = odom.pose.pose.position.y + np.random.normal(0, .1)
+            noise_odom.pose.pose.position.z = odom.pose.pose.position.z + np.random.normal(0, .1)
+            noise_odom.twist.twist.linear = odom.twist.twist.linear
+            noise_odom.twist.twist.angular = odom.twist.twist.angular
+
             self.auvs[auv][ODOM_INDEX] = odom
             self.auvs[auv][PUB_INDEX].publish(odom)
+            self.auvs[auv][NOISE_INDEX].publish(noise_odom)
 
     def correct_angles(self, angle):
         """ Map all angles between -pi to pi """
